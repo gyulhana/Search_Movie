d67f767ca6e7ec22dc84b53c1a409daf70ca0beb
gyulhana <matearz@korea.ac.kr>
2021-12-31 16:46:30 +0900

    🔧 fix: 모달 컴포넌트 분리

---
+++ src/components/MovieDetail.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="modal-dim" >
+    <div class="modal">
+      <img :src="details.Poster" onerror="this.src='https://i.imgur.com/1OpTile.png';" />
+      <div class="text-container">
+        <div>Title</div>
+        <div>Country</div>
+        <div>Language</div>
+        <div>Released</div>
+        <div>Runtime</div>
+        <div>Rated</div>
+        <div>Plot</div>
+      </div>
+      <div class="text-container">
+        <div>{{ details.Title }}</div>
+        <div>{{ details.Country }}</div>
+        <div>{{ details.Language }}</div>
+        <div>{{ details.Released }}</div>
+        <div>{{ details.Runtime }}</div>
+        <div>{{ details.Rated }}</div>
+        <div>{{ details.Plot }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ props: {
+   details: Object
+ }
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-dim {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  z-index: 1000;
+  background-color: rgba(0, 0, 0, 0.120);
+  .modal {
+    padding: 10rem;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: row;
+    background-color: transparent;
+    color: white; 
+    gap: 1rem;
+    font-size: 1.3rem;
+    img {
+      width: 20rem;
+      height: 25rem;
+    }
+    .text-container {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      padding: 1.3rem;
+      box-sizing: border-box;
+    }
+  }
+}
+</style>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -7,30 +7,9 @@
       <div class="movieTitle">{{ movielist.Title }},</div>
       <div>{{ movielist.Year }}</div>
     </div>
+    <MovieDetail :details="this.details" v-if="modal" @click.prevent="modalClose"/>
   </li>
-  <div v-if="modal" class="modal-dim" @click.prevent="modalClose">
-    <div class="modal">
-      <img :src="details.Poster" onerror="this.src='https://i.imgur.com/1OpTile.png';" />
-      <div class="text-container">
-        <div>Title</div>
-        <div>Country</div>
-        <div>Language</div>
-        <div>Released</div>
-        <div>Runtime</div>
-        <div>Rated</div>
-        <div>Plot</div>
-      </div>
-      <div class="text-container">
-        <div>{{ details.Title }}</div>
-        <div>{{ details.Country }}</div>
-        <div>{{ details.Language }}</div>
-        <div>{{ details.Released }}</div>
-        <div>{{ details.Runtime }}</div>
-        <div>{{ details.Rated }}</div>
-        <div>{{ details.Plot }}</div>
-      </div>
-    </div>
-    </div>
+
 </template>
 <script>
 import MovieDetail from './MovieDetail'
@@ -56,13 +35,11 @@ export default {
    movieDetail: async (key) => {
      const movieDetaildata = await fetch(`https://www.omdbapi.com?apikey=7035c60c&i=${key}&plot=full
     `, { method: "GET" }).then(res => res.json())
-    console.log(movieDetaildata)
     return movieDetaildata
    },
    async movieModal(key) {
      this.details = await this.movieDetail(key)
      this.modal = true
-     console.log(this.details)
 
    },
    modalClose() {
@@ -101,43 +78,4 @@ export default {
   white-space:nowrap;
   font-weight: 600;
 }
-
-.modal-dim {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100vw;
-  height: 100vh;
-  z-index: 1000;
-  background-color: rgba(0, 0, 0, 0.700);
-  .modal {
-    background-color: white;
-    padding: 10rem;
-    border-radius: 15px;
-    display: flex;
-    flex-direction: row;
-    background-color: transparent;
-    color: white; 
-    gap: 1rem;
-    font-size: 1.3rem;
-    img {
-      width: 20rem;
-      height: 25rem;
-    }
-    .text-container {
-      display: flex;
-      flex-direction: column;
-      gap: 1rem;
-      padding: 1.3rem;
-      box-sizing: border-box;
-    }
-  }
-}
-
 </style>
